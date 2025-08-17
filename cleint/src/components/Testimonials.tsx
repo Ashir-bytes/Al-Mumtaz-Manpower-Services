df7596dc320ec895e79d2ca4e78a9ef3d01873b2
Ashir-bytes <ashirhasnain29@gmail.com>
2025-08-18 00:06:37 +0500

    swiper

--- cleint/src/components/Testimonials.tsx
+++ cleint/src/components/Testimonials.tsx
@@ -1,5 +1,8 @@
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Pagination, Autoplay } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/pagination";
+
 
 const testimonials = [
     {
